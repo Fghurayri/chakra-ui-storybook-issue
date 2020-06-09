07e8b140d413efe50794c65b2a84a77d2f0a537c
Faisal Alghurayri <fghurayri@gmail.com>
2020-06-09 18:11:43 +0300

    add the fix.

---
+++ src/Button/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button } from "@chakra-ui/core";
+
+export default function ChakraButton() {
+  return <Button>Buttony</Button>;
+}
--- src/Button/story.js
+++ src/Button/story.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import { Button } from "@storybook/react/demo";
-import { Button } from "@chakra-ui/core";
+import Button from "./index";
 
 export default { title: "Button" };
 
